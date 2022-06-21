8fb837352a2c736107e47ae24c01b29b5d27b0f7
Anastasia Dmitrieva <dmitrieva.adm@students.dvfu.ru>
2022-06-21 22:19:10 +1000

    создать отправку писем и модель пользователя

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -8,18 +13,39 @@ const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
-// РОМЕЖУТОЧНОЕ ПО
+// ПРОМЕЖУТОЧНОЕ ПО
+// установить заголовки безопасности http
+app.use(helmet());
+
+// Логи запросов
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+// Ограничить запросы от одного IP
+app.use(
+  "/api",
+  rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: "Too many requests from this IP, please try again later.",
+  })
+);
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
+// Парсер тела запроса
+app.use(express.json({ limit: "10kb" }));
+
+// Стерилизация данных против инъекций NoSQL-запросов
+app.use(mongoSanitize());
+
+// Стерилизация данных против межсайтового скриптинга
+app.use(xss());
+
+// Предотвращение загрязнения параметрами
+app.use(hpp());
+
+// Статичные файлы
+app.use(express.static(`${__dirname}/public`));
 
 // МАРШРУТЫ
 app.use("/api/v1/recipes", recipeRouter);
